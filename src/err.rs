7622c3b3307c708764b5b91c8bc0390e1181ca20
Jeong YunWon <jeong@youknowone.org>
2025-04-21 16:28:09 +0900

    libm

--- src/err.rs
+++ src/err.rs
@@ -1,6 +1,20 @@
-// defined in libc
+// The values are defined in libc
 #[derive(Debug, PartialEq, Eq)]
 pub enum Error {
     EDOM = 33,
     ERANGE = 34,
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+impl TryFrom<libc::c_int> for Error {
+    type Error = libc::c_int;
+
+    fn try_from(value: libc::c_int) -> std::result::Result<Self, Self::Error> {
+        match value {
+            33 => Ok(Error::EDOM),
+            34 => Ok(Error::ERANGE),
+            _ => Err(value),
+        }
+    }
 }
